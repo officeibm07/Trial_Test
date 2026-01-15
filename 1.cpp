b808a2f0dff0533bbb3156d829f7f0a9e89c1f8b
Ravi Prakash <ravi@Ravis-MacBook-Pro.local>
2026-01-15 22:09:34 +0530

    second time branching with 2-changeBranch  and check merge conflict on 2.cpp file

--- 1.cpp
+++ 1.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     // this is demo
     // this has been done because of branching concept in 1.cpp
+    // second time branching has been done
     return 0;
 }
