1e0ff3ff871cf331dbeb5a0118aa2f32fd6ee8ad
Ravi Prakash <ravi@Ravis-MacBook-Pro.local>
2026-01-18 19:06:16 +0530

    changes on 3.cpp by main so that main can be ahead of 6-branch

--- 3.cpp
+++ 3.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     // this has been done while checking branching conecpt
+    // done after rebase by main branch to make main ahead of 6-branch
     return 0;
 }
